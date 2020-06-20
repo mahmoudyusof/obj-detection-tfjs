39f91acab318f555cbc6c73af69013e30382a6cc
mahmoudyusof <mahmoud.yusof27@gmail.com>
2020-06-20 20:36:37 +0200

    feat: getting webcam stream

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <web-cam></web-cam>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WebCam from "./components/WebCam.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    WebCam
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/WebCam.vue
@@ -0,0 +1,31 @@
+<template>
+  <video autoplay ref="vid" width="1280" height="720"></video>
+</template>
+
+<script>
+export default {
+  name: "web-cam",
+  mounted() {
+    navigator.getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+    navigator.getUserMedia(
+      { audio: false, video: { width: 1280, height: 720 } },
+      stream => {
+        this.$refs.vid.srcObject = stream;
+        this.$refs.vid.onloadedmetadata = e => {
+          video.play();
+        };
+      },
+      error => {
+        console.log(error);
+        this.$emit();
+      }
+    );
+  }
+};
+</script>
+
+<style>
+</style>
