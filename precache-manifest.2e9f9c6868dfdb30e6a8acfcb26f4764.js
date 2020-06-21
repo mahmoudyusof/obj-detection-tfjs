44ba30c84e30be7ea7e12b33336bdf173774d48f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-21 14:04:51 +0000

    Deploy mahmoudyusof/obj-detection-tfjs to github.com/mahmoudyusof/obj-detection-tfjs.git:gh-pages

---
+++ precache-manifest.2e9f9c6868dfdb30e6a8acfcb26f4764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858cdaaf6b82affeee2a",
+    "url": "/obj-detection-tfjs/css/app.d40fc157.css"
+  },
+  {
+    "revision": "8c6c032210a1a1dece53642c3ac8b98a",
+    "url": "/obj-detection-tfjs/index.html"
+  },
+  {
+    "revision": "858cdaaf6b82affeee2a",
+    "url": "/obj-detection-tfjs/js/app.803e1ea0.js"
+  },
+  {
+    "revision": "b0a571cf263563418773",
+    "url": "/obj-detection-tfjs/js/chunk-vendors.5de683e5.js"
+  },
+  {
+    "revision": "7f3fcd0e5b68e1ef73c3e33ae25c572a",
+    "url": "/obj-detection-tfjs/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/obj-detection-tfjs/robots.txt"
+  }
+]);
